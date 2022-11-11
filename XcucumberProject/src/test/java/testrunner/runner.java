f414e1264f5ae976b01dcdadaa0c2121ab854757
adithya <adithya@DESKTOP-QF44ST5>
2022-11-11 21:59:48 +0530

    first commit

---
+++ XcucumberProject/src/test/java/testrunner/runner.java
@@ -0,0 +1,19 @@
+package testrunner;
+
+import io.cucumber.junit.CucumberOptions;
+
+@CucumberOptions(features="C:\\Users\\adithya\\git\\repository4\\XcucumberProject\\src\\test\\resources\\Feature\\Login.feature",
+                glue="stepDefinition"
+                
+		
+		
+		
+		)
+
+
+
+
+
+public class runner {
+
+}
---
+++ XcucumberProject/src/test/resources/Feature/Login.feature
@@ -0,0 +1,7 @@
+Feature: Title of your feature
+
+  Scenario: s1
+  
+   Given g1
+   When w1
+   Then t1
